7972d9621eeb2fa22bab620fcb504743c24b85fc
Litran1990 <felipelitran@gmail.com>
2020-03-04 15:11:09 +0000

    Added Heroku and Postgres support for deployment

--- posts/views.py
+++ posts/views.py
@@ -12,7 +12,7 @@ def get_posts(request):
     """
     posts = Post.objects.filter(published_date__lte=timezone.now()
         ).order_by('-published_date')
-    return render(request, "blogposts.html", posts=posts)
+    return render(request, "blogposts.html", {'posts': posts})
 
 
 def post_detail(request, pk):
@@ -26,7 +26,7 @@ def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     post.views += 1
     post.save()
-    return render(request, "postdetail.html", post=post)
+    return render(request, "postdetail.html", {'post': post})
 
 
 def create_or_edit_post(request, pk=None):
@@ -43,4 +43,4 @@ def create_or_edit_post(request, pk=None):
             return redirect(post_detail, post.pk)
     else:
         form = BlogPostForm(instance=post)
-    return render(request, 'blogpostform.html', form=form)
+    return render(request, 'blogpostform.html', {'form': form})
